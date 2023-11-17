b7dcb25e17d5a145cfa263f66a37e5b7f6f8a530
cno7 <chr_iss@hotmail.com>
2023-11-17 23:24:58 +0100

    Rough outlining of idea
    Really rough outline of the idea for the site.

---
+++ baseScript.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    starting();
+}
+
+function starting() {
+    document.getElementById("mainIdContentLeft").innerHTML = "<ul><li><img src=\"images/blacksmithing/Trade_BlackSmithing.png\" alt=\"\">Blacksmithing</li><li>Engineering</li><li>Mining</li><li>Tailoring</li><li>Enchanting</li><li>Alchemy</li><li>Herbalism</li><li>First Aid</li><li>Fishing</li></ul>";
+
+    //document.getElementById("mainIdContentMiddle").innerHTML = "";
+    //document.getElementById("mainIdContentRight").innerHTML = "";
+}
